9d00c0a4cba4cf99870b516914a6ba61183dde6b
ggzhang <155051843@qq.com>
2019-11-15 04:15:43 +0000

    my gcn ok?

--- run_ContractionCoefficientsChanges_2gpus.sh
+++ run_ContractionCoefficientsChanges_2gpus.sh
@@ -2,9 +2,9 @@ dataset="Cora"
 
 name='Contraction_coefficients_changes'
 
-for i in 0.1 0.2
+for i in 0.1 0.2 0.4 0.8
 do
  echo "Contraction coefficients $i"
- python3 Mygcn.py  --ConCoeff $i --Epochs 20  2>&1 |tee Logs/${name}_${dataset}_${i}_$timestamp.log
+ python3 MyGCN.py  --ConCoeff $i --Epochs 10  2>&1 |tee Logs/${name}_${dataset}_${i}_$timestamp.log
 done
 
