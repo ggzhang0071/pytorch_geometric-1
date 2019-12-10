0b3c276fe3c2981562406f9d6b9d3a96ce05d98b
ggzhang <155051843@qq.com>
2019-12-10 06:20:47 +0000

    add PlotMonte calors convergence

--- Results/PlotMonteConvergence.py
+++ Results/PlotMonteConvergence.py
@@ -1,10 +1,12 @@
-coefficientsSecond=[4, 8, 16, 32, 64]
-coefficientsFirst=[16]
+coefficientsSecond=[0.95]
+coefficientsFirst=[1, 2, 4]
 dataset="Cora"
 file_constraited="TrainConvergence"
-save_png_name='ContractionCoefficientsCompare_{}.png'.format(dataset)
-start_plot=30
-epochs=70
+save_png_name='LayerNumCompare_{}.png'.format(dataset)
+start_plot=0
+epochs=200
+args=[3, 4,6]
+
 from PlotMonteCalorsConvergence import PlotMonteCalorsTimesConvergenceNpy
 
-PlotMonteCalorsTimesConvergenceNpy(dataset,file_constraited,coefficientsFirst,coefficientsSecond,save_png_name,start_plot,epochs)
+PlotMonteCalorsTimesConvergenceNpy(dataset,file_constraited,coefficientsFirst,coefficientsSecond,save_png_name,start_plot,epochs,*args)
