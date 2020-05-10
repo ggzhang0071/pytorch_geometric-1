f3f70654da12c183079a82fadecd68aaa0c948cc
ggzhang <155051843@qq.com>
2020-05-10 08:56:48 +0000

    add graph partition

--- Results/NNSpectralAnalysis.py
+++ Results/NNSpectralAnalysis.py
@@ -10,18 +10,18 @@ import pickle
 import scipy.sparse as sparse
 from visualization import RANDOM_STATE
 from spectral_cluster_model import clustering_experiment, weights_to_graph,cluster_net,delete_isolated_ccs,weights_array_to_cluster_quality
-from visualization import run_spectral_cluster,build_cluster_graph
+from visualization import run_spectral_cluster,build_cluster_graph,plot_eigenvalues
 
 
 dataset="Cora"
 file_constraited=dataset+"Convergence"
 
-for epoch in range(0,100,40):
+for epoch in range(0,200,40):
     weights_path0=dataset+"Convergence/WeightChanges-Cora-GCN-param_512_2_0.99_0.2-monte_0-"+str(epoch)+".pt"
     weights_array0=torch.load(weights_path0)
     weights_array=[]
     for (i, weight) in enumerate(weights_array0):
-        weight = weight[:100,:100].cpu().detach().numpy()
+        weight = weight[:800,:800].cpu().detach().numpy()
         weights_array.append(weight)
     weights_path=dataset+"Convergence/WeightChanges-Cora-GCN-param_512_2_0.99_0.2-monte_0-"+str(epoch)+".pckl"  
     pickle.dump(weights_array,open(weights_path,'wb'))
@@ -37,9 +37,9 @@ for epoch in range(0,100,40):
     #[ncut_val, clustering_labels]=weights_array_to_cluster_quality(weights_array, adj_mat, num_clusters,eigen_solver, assign_labels, epsilon,is_testing=False)
     #    weights_array_to_cluster_quality() 
     #[labels, metrics]=run_spectral_cluster(weights_path)
-
-    labels=cluster_net(num_clusters, adj_mat, eigen_solver, assign_labels)
-    build_cluster_graph(weights_path,labels,normalize_in_out=True)
+    plot_eigenvalues(weights_path)
+    #labels=cluster_net(num_clusters, adj_mat, eigen_solver, assign_labels)
+    #build_cluster_graph(weights_path,labels,normalize_in_out=True)
 
 
 
